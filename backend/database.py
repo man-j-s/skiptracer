fd1bd2da96c6e25ff1fd4ae85eeb25165f502dd6
man-j-s <macsngh99@gmail.com>
2026-08-06 02:23:15 -0400

    Database.py created

---
+++ backend/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from backend.config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
