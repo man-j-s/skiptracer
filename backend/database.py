3b431cb11c02c2a040eb24b5164f1034dfd53ce3
man-j-s <macsngh99@gmail.com>
2026-08-09 23:35:35 -0400

    Declared base class

--- backend/database.py
+++ backend/database.py
@@ -3,6 +3,5 @@ from sqlalchemy.ext.declarative import declarative_base
 from backend.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+Base = declarative_base()
