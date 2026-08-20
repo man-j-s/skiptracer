7fc81bae01c50b478d3fe03763ffbe8a154a7c71
man-j-s <macsngh99@gmail.com>
2026-08-19 23:58:29 -0400

    adding decorator for debtor search

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -2,14 +2,17 @@
 ###DEBTOR SEARCH FIELDS. THIS WILL SHOW THE OUTPUT##
 ####################################################
 
-from pydantic import Basemodel
+from pydantic import BaseModel
 from typing import Optional
+from fastapi import Depends
 
 class SearchDebtor(BaseModel):
-    name = str
-    city = str
-    province = str
-    phone = Optional[str] = None
-    employer = Optional[str] = None
-    email = Optional[str] = None
-    
+    name : str
+    city : str
+    province : str
+    phone : Optional[str] = None
+    employer : Optional[str] = None
+    email : Optional[str] = None
+
+@router.post("/search")
+def search(request : SearchDebtor):
