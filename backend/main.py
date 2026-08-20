4af710977a9638c73c0bec56bf661c1a0a1e6efb
man-j-s <macsngh99@gmail.com>
2026-08-20 02:36:13 -0400

    Search router initialisation. connecting with main.py

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,12 @@ from backend.models.search import Search #use name of class, not table
 from backend.models.result import Result #shows the search results
 from backend.models.email import Email
 from backend.routes.auth import router as auth_router
-
+from backend.routes.search import router as search_router #as is used to name
 
 app = FastAPI()
 Base.metadata.create_all(bind = engine)
-app.include_router(auth_router)
+app.include_router(auth_router) #include_router can only accept 1 router at a time & not a list
+app.include_router(search_router)
 
 # ***************************************************
 # ** IMPORTANT COMMANDS **
--- backend/routes/search.py
+++ backend/routes/search.py
@@ -4,7 +4,11 @@
 
 from pydantic import BaseModel
 from typing import Optional
-from fastapi import Depends
+from fastapi import Depends, APIRouter
+from backend.routes.auth import get_current_user
+from backend.models.user import User
+
+router = APIRouter()
 
 class SearchDebtor(BaseModel):
     name : str
@@ -15,4 +19,5 @@ class SearchDebtor(BaseModel):
     email : Optional[str] = None
 
 @router.post("/search")
-def search(request : SearchDebtor):
+def search(request : SearchDebtor, user_verify : User = Depends(get_current_user)):
+    return{"name" : request.name,  "city":request.city, "user": user_verify.email }
