2c0523a9b1904827c901d29c511aceab32cb55df
man-j-s <macsngh99@gmail.com>
2026-08-26 23:58:03 -0400

    testing search/history

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -24,16 +24,20 @@ class SearchDebtor(BaseModel):
 def search(request : SearchDebtor, user_verify : User = Depends(get_current_user)):
     db = SessionLocal()
     new_search = Search(user_id = user_verify.id, name = request.name, email = request.email, city = request.city, province = request.province, phone = request.phone, employer = request.employer)
-    db.add(new_search) #adding to the db
+    db.add(new_search) #creating Search row in db. 
     db.commit() #adding to the db.
     return{"search_id" : new_search.id, "name" : request.name,  "city":request.city, "user": user_verify.email }
 
 @router.get("/search/history")
-def search_history(user_verify : User = Depends(get_current_user), db = Depends(get_db)):
-    # db = SessionLocal() #start a db session but it does not closes it
-    # db = Depends(get_db) #not to include as separate variable inside function, as call will be done by now
-    db.query(Search).filter(Search.user_id == user_verify.id).all()   
-    return db.query(Search).filter(Search.user_id == user_verify.id).all()
+def search_history(user_verify: User = Depends(get_current_user)):
+    db = SessionLocal()
+    results = db.query(Search).filter(Search.user_id == user_verify.id).all()
+    return results
+# def search_history(user_verify : User = Depends(get_current_user), db = Depends(get_db)):
+#     # db = SessionLocal() #start a db session but it does not closes it
+#     # db = Depends(get_db) #not to include as separate variable inside function, as call will be done by now
+#     db.query(Search).filter(Search.user_id == user_verify.id).all()   
+#     return db.query(Search).filter(Search.user_id == user_verify.id).all()
 
 
 #SessionLocal() this is to start a db session. 
